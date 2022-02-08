6011d57f9845f2d03750000c94c8e73111ae6116
AdliXSec <naufalsyahruradli@gmail.com>
2022-02-08 10:57:12 +0700

    ip geo

--- belajar.py
+++ belajar.py
@@ -9,18 +9,18 @@ else:
     hapus = "clear"
 
 os.system(hapus)
-ipaddr = input(" Masukkan IP : ")
-ipreq  = requests.get(f"https://ip-api.com/json/{ipaddr}")
+ipaddr = input("Ip Address : ")
+ipreq = requests.get(f"http://ip-api.com/json/{ipaddr}")
 
 if ipreq.status_code == 200:
-    ipdata = json.loads(ipreq.text)
+	ipdata = json.loads(ipreq.text)
 
-    if ipdata["status"] == "success":
-        for key in ipdata:
-            print(f"{key.capitalize()} : {ipdata[key]}")
-            
-            if key == "lon":
-                lat = ipdata["lat"]
-                lon = ipdata["lon"]
-                maps = f"https://www.google.com/maps/@{lat},{lon},9z"
-                print(f" Maps : {maps}")
+	if ipdata["status"] == "success":
+		for key in ipdata:
+			print(f"{key.capitalize()} : {ipdata[key]}")
+			
+			if key == "lon":
+				lat = ipdata["lat"]
+				lon = ipdata["lon"]
+				maps = f"https://www.google.com/maps/@{lat},{lon},9z"
+				print(f"Maps : {maps}")
