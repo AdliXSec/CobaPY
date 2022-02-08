f9a237ae90b9b8f3ffa9c143fbadf05fcb990ef7
AdliXSec <naufalsyahruradli@gmail.com>
2022-02-08 13:07:18 +0700

    readme

---
+++ readme.py
@@ -0,0 +1,20 @@
+import time
+import os
+import platform
+
+if platform.system() == "Windows":
+    hapus = "cls"
+else:
+    hapus = "clear"
+
+os.system(hapus)
+print(" Warning :")
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+print(" Dilarang recode atau copy paste")
+time.sleep(0.1)
+print(" Dilarang Untuk disalah gunakan")
+time.sleep(0.1)
+print(" By AdliXSec X Ndndra")
+print("\n")
